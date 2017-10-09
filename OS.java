d78d461c39ca132f233a9329eae1836d808869a1
MikeBran1456 <michaelbranard@gmail.com>
2017-10-09 19:41:57 -0400

    integrated Rachel's OS class changes

--- OS.java
+++ OS.java
@@ -1,25 +1,27 @@
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.*;
-
-import javafx.scene.shape.Line; 
+import java.util.*; 
 
 class OS {
 	public CPU cpu;
 	public IOdevice io;
 	public boolean isCPUAvailable = true; // initially this had no value
 	// public ProcessTable process_Table;
-	public ArrayList<PCB> New_Queue;
-	public ArrayList<PCB> Ready_Queue;
-	public ArrayList<PCB> Wait_Queue;
-	public ArrayList<PCB> Terminated_Queue;
+	public ArrayList<Process> New_Queue;
+	public ArrayList<Process> Ready_Queue;
+	public ArrayList<Process> Wait_Queue;
+	public ArrayList<Process> Terminated_Queue;
 
+	// Read the txt input file, for each line, create a process and record its
+	// arrival
+	// time
+	// Put each process in New_Q queue initially then put them in Ready_Q
+	// Always check whether the CPU is idle or not; if yes, use your scheduler
+	// algorithm to select a process from the Ready_Queue for CPU execution\
+	// According to the return value of CPU execute(), put the process into the
+	// corresponding queue.
+	// Record the time of every operation for computing your latency and
+	// response
 	public void CreateProcess() throws NumberFormatException, IOException
 	{
 		/* 
@@ -33,27 +35,17 @@ class OS {
 		{
 			String[] words = line.split("," );
 			int ID = Integer.parseInt(words[0]); 
-			int priority = Integer.parseInt(words[2]); 
 			int arrival = Integer.parseInt(words[1]); 
+			int priority = Integer.parseInt(words[2]);  
 			String IOburst = words[3]; 
-			
-			ArrayList<String> burst = new ArrayList<>(); 
-			burst.add(IOburst); 
+			/*TODO split IOburst into integers and fill 
+			*an array and place as final argument in PCB constructor*/
+			PCB myPCB = new PCB(ID, arrival, priority, /* insert array here*/);
 		}
 		
 	}
+	
 	public void FCFS(Process process) {
-	// Read the txt input file, for each line, create a process and record its
-	// arrival
-	// time
-	// Put each process in New_Q queue initially then put them in Ready_Q
-	// Always check whether the CPU is idle or not; if yes, use your scheduler
-	// algorithm to select a process from the Ready_Queue for CPU execution\
-	// According to the return value of CPU execute(), put the process into the
-	// corresponding queue.
-	// Record the time of every operation for computing your latency and
-	// response
-	public void FCFS(PCB process) {
 		int timeslice = 99999;
 		boolean done = false;
 		CPU cpu = new CPU(timeslice);
@@ -83,8 +75,8 @@ class OS {
 				System.out.println("Executing process...");
 				CPU.BubbleSort(Ready_Queue.get(0));
 				Wait_Queue.add(Ready_Queue.get(0));
-				if (process.getBurstSequence() == null) {//This needs to be if(PCB.getBurstSequence()[pointOfNextInstruction] == PCB.getBurstSequence.size())
-					Wait_Queue.remove(0);//This may need to change based off of where its added to the queue
+				if (process.getBurstSequence() == null) {
+					Wait_Queue.remove(0);
 					state = "Terminated";
 				}
 				state = "Waiting";
@@ -109,7 +101,7 @@ class OS {
 		System.out.println("All processes have finished executing");
 	}
 
-	public void RoundRobin(PCB process) {
+	public void RoundRobin(Process process) {
 		int timeslice = 10;
 		boolean done = false;
 		CPU cpu = new CPU(timeslice);
@@ -166,7 +158,7 @@ class OS {
 		System.out.println("All processes have finished executing");
 	}
 
-	public void staticPriority(PCB process) {
+	public void staticPriority(Process process) {
 		int timeslice = 10;// This may need to change if we want to make
 							// the lower priority processes preemptive
 		boolean done = false;
@@ -223,8 +215,8 @@ class OS {
 		System.out.println("All processes have finished executing");
 	}
 
-	public void Circle(ArrayList<PCB> Ready_Queue) {
-		PCB head = Ready_Queue.get(0);
+	public void Circle(ArrayList<Process> Ready_Queue) {
+		Process head = Ready_Queue.get(0);
 		Ready_Queue.remove(0);
 		Ready_Queue.add(head);
 	}
