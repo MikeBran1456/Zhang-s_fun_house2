d84457fa27805f88a607d5c571fb7b9fd3ff7143
MikeBran1456 <michaelbranard@gmail.com>
2017-10-09 17:30:42 -0400

    added bubble sort

--- IOdevice.java
+++ IOdevice.java
@@ -1,19 +1,33 @@
-package simulation;
-
 import java.util.ArrayList;
 
 class IOdevice {
+	boolean BusyOrNot;
+	
 	public IOdevice(ArrayList<Process> Wait_Queue) {
 	}
 
-	public boolean BusyOrNot;
-
 	// Always pick one process from Wait_Queue to exeute
 	// public String execute(int IO_burst) { dont know what this does
-	BusyOrNot=true;
+	//BusyOrNot=true;
 	// Call Bubble Sort() for IO_burst times and then return ;
 	// }
 
-	public static void BubbleSort(Process Wait_Queue) {
+	public static void BubbleSort() {
+		double[] data = new double[1000];
+		for (int i = 0; i < 1000; i++){
+			data[i] = Math.random();
+		}
+		//Perform bubble sort
+		int n = data.length;
+        double temp = 0.0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j < (n - i); j++) {
+                if (data[j-1] < data[j]) {
+                    temp = data[j - 1];
+                    data[j - 1] = data[j];
+                    data[j] = temp;
+                }
+            }
+        }
 	}
 }
