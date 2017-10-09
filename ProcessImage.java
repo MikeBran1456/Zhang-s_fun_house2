9388fe9f0a7759716048ba8369fd95313b861868
MikeBran1456 <michaelbranard@gmail.com>
2017-10-09 16:01:01 -0400

    moved things to PCB

--- ProcessImage.java
+++ ProcessImage.java
@@ -5,41 +5,6 @@
  */
 
 class ProcessImage {
-	//instance of PCB for data of a process
-	PCB Pcb_data;
-	//priority integer for later (not sure yet)
-	int Priority;
-	//variables for time calculation later
-	long ProcessCreated;
-	long ProcessFinished;
-	long ProcessIOComplete;
-	long Latency;
-	long Response;
-	//constructor
-	public ProcessImage(int id, int arrivalTime, int priority, int[] IOSeq) {
-		this.Pcb_data = new PCB(id, arrivalTime, "New", IOSeq);
-		this.Priority = priority;
-		this.ProcessCreated = System.currentTimeMillis();
-	}
-	//time calcs
-	public void setProcessFinished(long time){
-		this.ProcessFinished = time;
-	}
-	//time calcs
-	public void setProcessIOComplete(long time){
-		this.ProcessIOComplete = time;
-	}
-	//time calcs
-	public int getLatency(){
-		int TotalLatency = 0;
-		TotalLatency = (int)(this.ProcessCreated-this.ProcessFinished);
-		return TotalLatency;
-	}
-	//time calcs
-	public int getResponseTime(){
-		int TotalResponseTime = 0;
-		TotalResponseTime = (int)(this.ProcessCreated - this.ProcessIOComplete);
-		return TotalResponseTime;
-	}
-	//TODO possibly add methods to calculate various features.
+	
+	//TODO unknown
 }
