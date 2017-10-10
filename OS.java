48271590b7bc47be25c4178cd871514473178776
porter235 <31319959+porter235@users.noreply.github.com>
2017-10-10 12:41:54 -0400

    Os timing changes

--- OS.java
+++ OS.java
@@ -56,6 +56,7 @@ class OS {
 	public void FCFS(PCB process) {
 		int timeslice = 99999;
 		boolean done = false;
+		boolean firstIOCheck = true;
 		CPU cpu = new CPU(timeslice);
 
 		while (!done) {
@@ -85,7 +86,7 @@ class OS {
 				System.out.println("Executing process...");
 				CPU.BubbleSort();
 				Wait_Queue.add(Ready_Queue.get(0));
-				if (process.getBurstSequence() == null) {
+				if (process.getNextInstruction() == process.getBurstSequence().length){
 					Wait_Queue.remove(0);
 					state = "Terminated";
 					process.setProcessState("Terminated");
@@ -97,11 +98,16 @@ class OS {
 			case "Waiting":// Process is waiting on I/O
 				System.out.println("Waiting on I/O");
 				IOdevice.BubbleSort();
+				if(firstIOCheck == true){
+					process.setProcessIOComplete();
+					firstIOCheck = false;
+				}
 				state = "Running";
 				process.setProcessState("Running");
 
 			case "Terminated":// Process has finished executing
 				Terminated_Queue.add(Ready_Queue.get(0));
+				process.setProcessFinished();//Sets the finish time
 				Ready_Queue.remove(0);
 				if (Ready_Queue == null) {
 					done = true;// If there are no more processes to execute its
@@ -119,6 +125,7 @@ class OS {
 	public void RoundRobin(PCB process) {
 		int timeslice = 10;
 		boolean done = false;
+		boolean firstIOCheck = true;
 		CPU cpu = new CPU(timeslice);
 
 		while (!done) {
@@ -147,7 +154,7 @@ class OS {
 				System.out.println("Executing process...");
 				CPU.BubbleSort();
 				Wait_Queue.add(Ready_Queue.get(0));
-				if (process.getBurstSequence() == null) {// Need to circle queue
+				if (process.getNextInstruction() == process.getBurstSequence().length) {// Need to circle queue
 															// if timeslice is
 															// surpasses
 					Wait_Queue.remove(0);
@@ -160,11 +167,16 @@ class OS {
 			case "Waiting":// Process is waiting on I/O
 				System.out.println("Waiting on I/O");
 				IOdevice.BubbleSort();
+				if(firstIOCheck == true){
+					process.setProcessIOComplete();
+					firstIOCheck = false;
+				}
 				state = "Running";
 				process.setProcessState("Running");
 
 			case "Terminated":// Process has finished executing
 				Terminated_Queue.add(Ready_Queue.get(0));
+				process.setProcessFinished();
 				Ready_Queue.remove(0);
 				if (Ready_Queue == null) {
 					done = true;// If there are no more processes to execute its
@@ -183,6 +195,7 @@ class OS {
 		int timeslice = 10;// This may need to change if we want to make
 							// the lower priority processes preemptive
 		boolean done = false;
+		boolean firstIOCheck = true;
 		CPU cpu = new CPU(timeslice);
 
 		while (!done) {
@@ -212,7 +225,7 @@ class OS {
 				System.out.println("Executing process...");
 				CPU.BubbleSort();
 				Wait_Queue.add(Ready_Queue.get(0));
-				if (process.getBurstSequence() == null) {
+				if (process.getNextInstruction() == process.getBurstSequence().length) {
 					Wait_Queue.remove(0);
 					state = "Terminated";
 					process.setProcessState("Terminated");
@@ -223,11 +236,16 @@ class OS {
 			case "Waiting":// Process is waiting on I/O
 				System.out.println("Waiting on I/O");
 				IOdevice.BubbleSort();
+				if(firstIOCheck == true){
+					process.setProcessIOComplete();
+					firstIOCheck = false;
+				}
 				state = "Running";
 				process.setProcessState("Running");
 
 			case "Terminated":// Process has finished executing
 				Terminated_Queue.add(Ready_Queue.get(0));
+				process.setProcessFinished();
 				Ready_Queue.remove(0);
 				if (Ready_Queue == null) {
 					done = true;// If there are no more processes to execute its
@@ -246,59 +264,6 @@ class OS {
 		PCB head = Ready_Queue.get(0);
 		Ready_Queue.remove(0);
 		Ready_Queue.add(head);
-	}
-	//this calculates average for a double array
-	public double avg(double[] c){
-		//create variables for averaging
-		double total = 0;
-		int members = c.length;
-		double avg;
-		//calculate Average of input variable
-		for (int i = 0; i < members; i++){
-			total = total + c[i];
-		}
-		avg = total/members;
-		//return average
-		System.out.println("Average of run times: " + avg + " ms");
-		return avg;
-	}
-	//this calculates standard deviation for a double array
-	public double standardDev(double[] c){
-		//create all variables needed for calculating standard Deviation
-		double standardDev = 0.0;
-		int arrayLength = c.length;
-		double valuesAvg = avg(c);
-		double number = 0.0;
-		double sampleVariance = 0.0;
-		//pull apart formula into summation, division, and square root
-		for (int i = 0; i < arrayLength; i++){
-			number = number +  (c[i] - valuesAvg)*(c[i] - valuesAvg);
-		}
-		sampleVariance = number/(arrayLength-1);
-		standardDev = Math.sqrt(sampleVariance);
-		//return standard deviation
-		System.out.println("Standard Deviation of run times: " + standardDev + " ms");
-		return standardDev;
-	}
-	//this method calculates, prints, and returns the standard deviation of the latency times for all finished processes
-	public double getLatencyTime(){
-		int size = Terminated_Queue.size();
-		double[] latTimes = new double[size];
-		for (int i = 0; i < size; i++){
-			latTimes[i] = (double)Terminated_Queue.get(i).getLatency();
-		}
-		double latencyDev = standardDev(latTimes);
-		return latencyDev;
-	}
-	//this method calculates, prints, and returns the standard deviation of the response times for all finished processes
-	public double getResponseTime(){
-		int size = Terminated_Queue.size();
-		double[] resTimes = new double[size];
-		for (int i = 0; i < size; i++){
-			resTimes[i] = (double)Terminated_Queue.get(i).getResponseTime();
-		}
-		double responseDev = standardDev(resTimes);
-		return responseDev;
 	}
 
 } 
