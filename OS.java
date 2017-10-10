86bb12755bb65970589aaa1f4167fc3cc2217fdf
MikeBran1456 <michaelbranard@gmail.com>
2017-10-09 21:23:26 -0400

    added state changers to sorting algorithms

--- OS.java
+++ OS.java
@@ -1,5 +1,6 @@
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.*;
 import java.util.*; 
 
 class OS {
@@ -7,10 +8,10 @@ class OS {
 	public IOdevice io;
 	public boolean isCPUAvailable = true; // initially this had no value
 	// public ProcessTable process_Table;
-	public ArrayList<Process> New_Queue;
-	public ArrayList<Process> Ready_Queue;
-	public ArrayList<Process> Wait_Queue;
-	public ArrayList<Process> Terminated_Queue;
+	public ArrayList<PCB> New_Queue;
+	public ArrayList<PCB> Ready_Queue;
+	public ArrayList<PCB> Wait_Queue;
+	public ArrayList<PCB> Terminated_Queue;
 
 	// Read the txt input file, for each line, create a process and record its
 	// arrival
@@ -45,7 +46,7 @@ class OS {
 		
 	}
 	
-	public void FCFS(Process process) {
+	public void FCFS(PCB process) {
 		int timeslice = 99999;
 		boolean done = false;
 		CPU cpu = new CPU(timeslice);
@@ -63,28 +64,34 @@ class OS {
 
 				System.out.println("Creating process...");
 				state = "Ready";
+				process.setProcessState("Ready");
 
 			case "Ready":// Process is ready to be executed
 				System.out.println("Process ready to execute...");
 				if (isCPUAvailable == true) {
 					state = "Running";
+					process.setProcessState("Running");
 				}
 				System.out.println("CPU is not available, please wait...");
 
 			case "Running":// Process is now being executed
 				System.out.println("Executing process...");
-				CPU.BubbleSort(Ready_Queue.get(0));
+				CPU.BubbleSort();
 				Wait_Queue.add(Ready_Queue.get(0));
 				if (process.getBurstSequence() == null) {
 					Wait_Queue.remove(0);
 					state = "Terminated";
+					process.setProcessState("Terminated");
+					
 				}
 				state = "Waiting";
+				process.setProcessState("Waiting");
 
 			case "Waiting":// Process is waiting on I/O
 				System.out.println("Waiting on I/O");
-				IOdevice.BubbleSort(Wait_Queue.get(0));
+				IOdevice.BubbleSort();
 				state = "Running";
+				process.setProcessState("Running");
 
 			case "Terminated":// Process has finished executing
 				Terminated_Queue.add(Ready_Queue.get(0));
@@ -95,13 +102,14 @@ class OS {
 				}
 				System.out.println("Finished executing");
 				state = "Ready";
+				process.setProcessState("Ready");
 				break;
 			}
 		}
 		System.out.println("All processes have finished executing");
 	}
 
-	public void RoundRobin(Process process) {
+	public void RoundRobin(PCB process) {
 		int timeslice = 10;
 		boolean done = false;
 		CPU cpu = new CPU(timeslice);
@@ -118,30 +126,35 @@ class OS {
 				}
 				System.out.println("Creating process...");
 				state = "Ready";
+				process.setProcessState("Ready");
 
 			case "Ready":// Process is ready to be executed
 				System.out.println("Process ready to execute...");
 				if (isCPUAvailable == true) {
 					state = "Running";
+					process.setProcessState("Running");
 				}
 				System.out.println("CPU is not available, please wait...");
 
 			case "Running":// Process is now being executed
 				System.out.println("Executing process...");
-				CPU.BubbleSort(Ready_Queue.get(0));
+				CPU.BubbleSort();
 				Wait_Queue.add(Ready_Queue.get(0));
 				if (process.getBurstSequence() == null) {// Need to circle queue
 															// if timeslice is
 															// surpasses
 					Wait_Queue.remove(0);
 					state = "Terminated";
+					process.setProcessState("Terminated");
 				}
 				state = "Waiting";
+				process.setProcessState("Waiting");
 
 			case "Waiting":// Process is waiting on I/O
 				System.out.println("Waiting on I/O");
-				IOdevice.BubbleSort(Wait_Queue.get(0));
+				IOdevice.BubbleSort();
 				state = "Running";
+				process.setProcessState("Running");
 
 			case "Terminated":// Process has finished executing
 				Terminated_Queue.add(Ready_Queue.get(0));
@@ -152,13 +165,14 @@ class OS {
 				}
 				System.out.println("Finished executing");
 				state = "Ready";
+				process.setProcessState("Ready");
 				break;
 			}
 		}
 		System.out.println("All processes have finished executing");
 	}
 
-	public void staticPriority(Process process) {
+	public void staticPriority(PCB process) {
 		int timeslice = 10;// This may need to change if we want to make
 							// the lower priority processes preemptive
 		boolean done = false;
@@ -177,28 +191,33 @@ class OS {
 
 				System.out.println("Creating process...");
 				state = "Ready";
+				process.setProcessState("Ready");
 
 			case "Ready":// Process is ready to be executed
 				System.out.println("Process ready to execute...");
 				if (isCPUAvailable == true) {
 					state = "Running";
+					process.setProcessState("Running");
 				}
 				System.out.println("CPU is not available, please wait...");
 
 			case "Running":// Process is now being executed
 				System.out.println("Executing process...");
-				CPU.BubbleSort(Ready_Queue.get(0));
+				CPU.BubbleSort();
 				Wait_Queue.add(Ready_Queue.get(0));
 				if (process.getBurstSequence() == null) {
 					Wait_Queue.remove(0);
 					state = "Terminated";
+					process.setProcessState("Terminated");
 				}
 				state = "Waiting";
+				process.setProcessState("Waiting");
 
 			case "Waiting":// Process is waiting on I/O
 				System.out.println("Waiting on I/O");
-				IOdevice.BubbleSort(Wait_Queue.get(0));
+				IOdevice.BubbleSort();
 				state = "Running";
+				process.setProcessState("Running");
 
 			case "Terminated":// Process has finished executing
 				Terminated_Queue.add(Ready_Queue.get(0));
@@ -209,16 +228,17 @@ class OS {
 				}
 				System.out.println("Finished executing");
 				state = "Ready";
+				process.setProcessState("Ready");
 				break;
 			}
 		}
 		System.out.println("All processes have finished executing");
 	}
 
-	public void Circle(ArrayList<Process> Ready_Queue) {
-		Process head = Ready_Queue.get(0);
+	public void Circle(ArrayList<PCB> Ready_Queue) {
+		PCB head = Ready_Queue.get(0);
 		Ready_Queue.remove(0);
 		Ready_Queue.add(head);
 	}
 
-}
+} 
